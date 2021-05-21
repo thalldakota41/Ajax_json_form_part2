e3dd27a7e247d8e7857a2fc1928e0bc30d87ae42
Tony Hall <tonyhall@tonys-mbp.lan>
2021-05-20 21:09:27 -0400

    first commit

---
+++ user_login/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.index),
+    path('all.son', views.all_json),
+    path('all.html', views.all_html),
+    path('find', views.find),
+]
